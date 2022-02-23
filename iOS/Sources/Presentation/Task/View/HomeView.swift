a94683636ab840044606cf92604c831dd9d77e99
baegteun <baegteun@gmail.com>
2022-02-24 02:29:14 +0900

    :sparkles: :: 주간리스트까지

--- iOS/Sources/Presentation/Task/View/HomeView.swift
+++ iOS/Sources/Presentation/Task/View/HomeView.swift
@@ -9,11 +9,41 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject var viewModel: TaskVM = AppDelegate.container.resolve(TaskVM.self)!
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
                 Section {
-                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 10) {
+                            ForEach(viewModel.currentWeek, id: \.self) { day in
+                                VStack(spacing: 10) {
+                                    Text(day.extractDate(format: "dd"))
+                                        .font(.system(size: 14))
+                                        .fontWeight(.semibold)
+                                    
+                                    Text(day.extractDate(format: "EEE"))
+                                        .font(.system(size: 14))
+                                    
+                                    if day.isToday() {
+                                        Circle()
+                                            .fill(.white)
+                                            .frame(width: 8, height: 8)
+                                    }
+                                }
+                                .foregroundColor(.white)
+                                .frame(width: 45, height: 90)
+                                .background(
+                                    ZStack {
+                                        Capsule()
+                                            .fill(.black)
+                                        
+                                    }
+                                )
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
                 } header: {
                     HeaderView()
                 }
@@ -29,18 +59,33 @@ struct HomeView: View {
                     Text(Date().formatted(date: .abbreviated, time: .omitted))
                         .foregroundColor(.gray)
                 } else {
-                    Text(Date().formatString())
+                    Text(Date().extractDate(format: "EEE m, yyyy"))
+                        .foregroundColor(.gray)
                 }
                 Text("Today")
                     .font(.largeTitle.bold())
             }
+            .hLeading()
             
+            Button {
+                
+            } label: {
+                Image("Baekteun")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 45, height: 45)
+                    .clipShape(Circle())
+            }
         }
+        .padding()
+        .background(Color.white)
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        Group{
+            HomeView()
+        }
     }
 }
