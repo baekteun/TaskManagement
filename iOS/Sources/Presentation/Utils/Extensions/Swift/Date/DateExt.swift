719f7d18382f33f73179f6cb4bb6a992b3e82f6d
baegteun <baegteun@gmail.com>
2022-02-27 10:24:11 +0900

    :sparkles: :: isToday timezone

--- iOS/Sources/Presentation/Utils/Extensions/Swift/Date/DateExt.swift
+++ iOS/Sources/Presentation/Utils/Extensions/Swift/Date/DateExt.swift
@@ -4,10 +4,13 @@ extension Date {
     func extractDate(format: String) -> String{
         let formatter = DateFormatter()
         formatter.dateFormat = format
+        formatter.locale = Locale(identifier: "Ko_kr")
+        formatter.timeZone = TimeZone(identifier: "UTC")
         return formatter.string(from: self)
     }
     func isToday() -> Bool {
-        let cal = Calendar.current
+        var cal = Calendar.current
+        cal.timeZone = TimeZone(identifier: "UTC")!
         return cal.isDateInToday(self)
     }
 }
