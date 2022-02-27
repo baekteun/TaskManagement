a2ee93d2aaedfc0939d9ded4b0777e4335c64b1e
baegteun <baegteun@gmail.com>
2022-02-27 11:18:01 +0900

    :sparkles: :: Hoem

--- iOS/Sources/Presentation/Task/VM/TaskVM.swift
+++ iOS/Sources/Presentation/Task/VM/TaskVM.swift
@@ -1,16 +1,17 @@
 import Combine
 import Service
 import Foundation
+import SwiftUI
 
 final class TaskVM: ObservableObject {
     // MARK: - Properties
     @Published var tasks: [BTask] = [
+        .init(taskTitle: "Meeting", taskDescription: "Discuss team task for the day", taskDate: Date()),
+        .init(taskTitle: "Icon set", taskDescription: "Edit icons for team task for next week", taskDate: .init(timeIntervalSince1970: 16416_56822)),
+        .init(taskTitle: "Prototype", taskDescription: "Make and send prototype", taskDate: .init(timeIntervalSinceNow: 8200)),
         .init(taskTitle: "Meeting", taskDescription: "Discuss team task for the day", taskDate: .init(timeIntervalSince1970: 16416_41927)),
         .init(taskTitle: "Icon set", taskDescription: "Edit icons for team task for next week", taskDate: .init(timeIntervalSince1970: 16416_56822)),
-        .init(taskTitle: "Prototype", taskDescription: "Make and send prototype", taskDate: .init(timeIntervalSince1970: 16416_29381)),
-        .init(taskTitle: "Meeting", taskDescription: "Discuss team task for the day", taskDate: .init(timeIntervalSince1970: 16416_41927)),
-        .init(taskTitle: "Icon set", taskDescription: "Edit icons for team task for next week", taskDate: .init(timeIntervalSince1970: 16416_56822)),
-        .init(taskTitle: "Prototype", taskDescription: "Make and send prototype", taskDate: .init(timeIntervalSince1970: 16416_29381)),
+        .init(taskTitle: "Prototype", taskDescription: "Make and send prototype", taskDate: Date()),
         .init(taskTitle: "Icon set", taskDescription: "Edit icons for team task for next week", taskDate: .init(timeIntervalSince1970: 16416_56822)),
         .init(taskTitle: "Prototype", taskDescription: "Make and send prototype", taskDate: .init(timeIntervalSince1970: 16416_29381))
     ]
@@ -18,12 +19,27 @@ final class TaskVM: ObservableObject {
     
     @Published var currentDate = Date()
     
+    @Published var filteredTasks: [BTask]?
+    
     // MARK: - Init
     init() {
         fetchCurrentWeek()
     }
     
     // MARK: - Method
+    func filterTodayTasks() {
+        DispatchQueue.global(qos: .userInteractive).async {
+            var calendar = Calendar.current
+            calendar.timeZone = TimeZone(identifier: "UTC")!
+            
+            let filtered = self.tasks.filter{ calendar.isDate($0.taskDate, inSameDayAs: self.currentDate) }.sorted { $0.taskDate < $1.taskDate }
+            DispatchQueue.main.async {
+                withAnimation {
+                    self.filteredTasks = filtered
+                }
+            }
+        }
+    }
     func fetchCurrentWeek() {
         let today = Date()
         let calendar = Calendar.current
@@ -46,5 +62,10 @@ final class TaskVM: ObservableObject {
         calendar.timeZone = TimeZone(identifier: "UTC")!
         return calendar.isDate(date, inSameDayAs: currentDate)
     }
-    
+    func isCurrentHour(_ date: Date) -> Bool {
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let currentHour = calendar.component(.hour, from: Date())
+        return hour == currentHour
+    }
 }
--- iOS/Sources/Presentation/Task/View/HomeView.swift
+++ iOS/Sources/Presentation/Task/View/HomeView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Service
 
 struct HomeView: View {
     @StateObject var viewModel: TaskVM = AppDelegate.container.resolve(TaskVM.self)!
@@ -53,14 +54,124 @@ struct HomeView: View {
                         }
                         .padding(.horizontal)
                     }
+                    
+                    TasksView()
                 } header: {
                     HeaderView()
                 }
 
             }
         }
+        .ignoresSafeArea(.container, edges: .top)
     }
     
+    // MARK: TasksView
+    func TasksView() -> some View {
+        LazyVStack(spacing: 20) {
+            if let tasks = viewModel.filteredTasks {
+                if tasks.isEmpty {
+                    Text("No task found")
+                        .font(.system(size: 16))
+                        .fontWeight(.light)
+                        .offset(y: 100)
+                } else {
+                    ForEach(tasks, id: \.id) { task in
+                        TaskCardView(task: task)
+                    }
+                }
+            } else {
+                ProgressView()
+                    .offset(y: 100)
+            }
+        }
+        .padding()
+        .padding(.top)
+        .onChange(of: viewModel.currentDate) { newValue in
+            viewModel.filterTodayTasks()
+        }
+    }
+    
+    // MARK: - TaskCardView
+    func TaskCardView(task: BTask) -> some View {
+        HStack(alignment: .top, spacing: 30) {
+            VStack {
+                Circle()
+                    .fill(viewModel.isCurrentHour(task.taskDate) ? .black : .clear)
+                    .frame(width: 15, height: 15)
+                    .background(
+                        Circle()
+                            .stroke(.black, lineWidth: 1)
+                            .padding(-3)
+                    )
+                    .scaleEffect(!viewModel.isCurrentHour(task.taskDate) ? 0.8 : 1)
+                Rectangle()
+                    .fill(.black)
+                    .frame(width: 3)
+            }
+            VStack {
+                HStack(alignment: .top, spacing: 10) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text(task.taskTitle)
+                            .font(.title2.bold())
+                        Text(task.taskDescription)
+                            .font(.callout)
+                    }
+                    .hLeading()
+                    
+                    Text(task.taskDate.extractDate(format: "hh:MM a"))
+                }
+                
+                // MARK: Memeber
+                if viewModel.isCurrentHour(task.taskDate) {
+                    HStack(spacing: 0) {
+                        HStack(spacing: -10) {
+                            ForEach([
+                                "https://avatars.githubusercontent.com/u/63863135?s=100&v=4",
+                                "https://avatars.githubusercontent.com/u/28175067?s=100&v=4"],
+                                    id: \.self) { user in
+                                Image(uiImage: URL(string: user)?.toImage() ?? .init())
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 45, height: 45)
+                                    .clipShape(Circle())
+                                    .background(
+                                        Circle()
+                                            .stroke(.black, lineWidth: 5)
+                                    )
+                            }
+                        }
+                        .hLeading()
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.black)
+                                .padding(10)
+                                .background(
+                                    Rectangle()
+                                        .background(Color.white)
+                                        .cornerRadius(10)
+                                        .clipped()
+                                    
+                                )
+                        }
+                    }
+                    .padding(.top)
+                }
+            }
+            .foregroundColor(viewModel.isCurrentHour(task.taskDate) ? .white : .black)
+            .padding(viewModel.isCurrentHour(task.taskDate) ? 15 : 0)
+            .padding(.bottom, viewModel.isCurrentHour(task.taskDate) ? 0 : 10)
+            .hLeading()
+            .background(
+                Color.black
+                    .cornerRadius(25)
+                    .opacity(viewModel.isCurrentHour(task.taskDate) ? 1 : 0)
+            )
+        }
+        .hLeading()
+    }
+    // MARK: HeaderView
     func HeaderView() -> some View {
         HStack(spacing: 10) {
             VStack(alignment: .leading, spacing: 10) {
@@ -86,6 +197,7 @@ struct HomeView: View {
                     .clipShape(Circle())
             }
         }
+        .padding(.top, getSafeArea().top)
         .padding()
         .background(Color.white)
     }
