9437a7ecf337cb30236cdbaa96e1eecbc75d3075
baegteun <baegteun@gmail.com>
2022-02-27 11:17:46 +0900

    :sparkles: :: URL to UIImage

---
+++ iOS/Sources/Presentation/Utils/Extensions/Swift/URL/URLExt.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+extension URL {
+    func toImage() -> UIImage {
+        let data = (try? Data(contentsOf: self))!
+        return UIImage.init(data: data) ?? UIImage()
+    }
+}
