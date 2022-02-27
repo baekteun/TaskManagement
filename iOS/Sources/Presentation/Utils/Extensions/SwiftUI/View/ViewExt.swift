6d95614fd20d9c70f74190d0f53d72e8cf7f6592
baegteun <baegteun@gmail.com>
2022-02-27 11:18:14 +0900

    :sparkles: :: getSafeArea

--- iOS/Sources/Presentation/Utils/Extensions/SwiftUI/View/ViewExt.swift
+++ iOS/Sources/Presentation/Utils/Extensions/SwiftUI/View/ViewExt.swift
@@ -13,4 +13,13 @@ extension View {
         self
             .frame(maxWidth: .infinity, alignment: .center)
     }
+    func getSafeArea() -> UIEdgeInsets {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .zero
+        }
+        guard let safeArea = screen.windows.first?.safeAreaInsets else {
+            return .zero
+        }
+        return safeArea
+    }
 }
